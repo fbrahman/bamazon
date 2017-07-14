d40bfe79386069e555939d7536b8586e72af1b5b
fbrahman <fbrahman@gmail.com>
2017-07-14 16:10:37 -0700

    Added validation for user input

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,14 +9,17 @@ const bamazonCustomer = (function() {
 
     connection.connect();
 
+    let idMax = 0;
+
     let snapQuery = `SELECT item_id AS ID
-    					, product_name AS Product
-    					, price AS Price
-    				 FROM bamazon.products`
+                        , product_name AS Product
+                        , price AS Price
+                     FROM bamazon.products`
 
     connection.query(snapQuery, function(err, results, fields) {
         if (err) return console.log(err);
-        console.log('\n'+Table.print(results));
+        idMax = results.length;
+        console.log('\n' + Table.print(results));
         inquirerInitial();
     });
 
@@ -24,7 +27,14 @@ const bamazonCustomer = (function() {
         inquirer.prompt([{
             type: 'input',
             name: 'itemID',
-            message: 'Please enter the id of the item you would like to purchase:'
+            message: 'Please enter the id of the item you would like to purchase:',
+            validate: function(num){
+                if (!isNaN(num) && num > 0 && num <= idMax){
+                    return true;
+                } else {
+                    return false;
+                }
+            }
         }, {
             type: 'input',
             name: 'quantityRequested',
@@ -34,49 +44,55 @@ const bamazonCustomer = (function() {
             let quantityReq = userInput.quantityRequested;
 
             let reqQuery = `SELECT item_id AS ID
-    							, product_name AS Product
-    							, price AS Price
-    							, stock_quantity AS Stock
-    							, product_sales AS Sales
-    						FROM bamazon.products
-    						WHERE item_id = ?`;
+                                , product_name AS Product
+                                , price AS Price
+                                , stock_quantity AS Stock
+                                , product_sales AS Sales
+                            FROM bamazon.products
+                            WHERE item_id = ?`;
             let reqValues = [itemID];
 
             connection.query(reqQuery, reqValues, function(err, results) {
 
-                let currentStock = results[0].Stock;
-                let productName = results[0].Product;
-                let itemID = results[0].ID;
-                let price = results[0].Price;
-                let currSales = results[0].Sales;
-
                 if (err) return console.log(err);
-                if (currentStock < quantityReq) {
-                    console.log(`\nWe currently do not have ${quantityReq} ${productName}(s) available for purchase. Please check in again at a later time.`)
-                    connection.end();
-                } else if (currentStock >= quantityReq) {
-                    let newStockLevel = currentStock - quantityReq;
-                    let totalCost = quantityReq * price;
-                    let totalSales = currSales + totalCost;
 
-                    let updateQuery = `UPDATE bamazon.products
-                						  SET stock_quantity = ?
-                						  	  , product_sales = ?
-                						WHERE item_id = ?`;
-                    let updateValue = [newStockLevel, totalSales, itemID]
+                if (results.length) {
+                    let currentStock = results[0].Stock;
+                    let productName = results[0].Product;
+                    let itemID = results[0].ID;
+                    let price = results[0].Price;
+                    let currSales = results[0].Sales;
 
-                    connection.query(updateQuery, updateValue, function(err, results) {
-
-                        if (err) return console.log(err);
-
-                        if (results.changedRows === 0) {
-                            console.log('Boo something did\'t work');
-                        } else if (results.changedRows > 0) {
-                            let resultsArray = [{ 'Product': productName, 'Quantity': quantityReq, 'Total': totalCost }]
-                            console.log('\n'+Table.print(resultsArray));
-                        };
+                    if (currentStock < quantityReq) {
+                        console.log(`\nWe currently do not have ${quantityReq} ${productName}(s) available for purchase. Please check in again at a later time.`)
                         connection.end();
-                    })
+                    } else if (currentStock >= quantityReq) {
+                        let newStockLevel = currentStock - quantityReq;
+                        let totalCost = quantityReq * price;
+                        let totalSales = currSales + totalCost;
+
+                        let updateQuery = `UPDATE bamazon.products
+                                              SET stock_quantity = ?
+                                                  , product_sales = ?
+                                            WHERE item_id = ?`;
+                        let updateValue = [newStockLevel, totalSales, itemID]
+
+                        connection.query(updateQuery, updateValue, function(err, results) {
+
+                            if (err) return console.log(err);
+
+                            if (results.changedRows === 0) {
+                                console.log('Boo something did\'t work');
+                            } else if (results.changedRows > 0) {
+                                let resultsArray = [{ 'Product': productName, 'Quantity': quantityReq, 'Total': totalCost }]
+                                console.log('\n' + Table.print(resultsArray));
+                            };
+                            connection.end();
+                        })
+                    }
+                } else {
+                    console.log(`\nI'm sorry that item does not exist. Thank you please visit again.`);
+                    connection.end();
                 }
             })
         })
